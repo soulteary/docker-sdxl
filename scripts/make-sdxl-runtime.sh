a8bf61fb49936e3d0405725d5500e4074fcd8280
soulteary <soulteary@gmail.com>
2023-07-28 20:53:16 +0800

    fix: xformers-runtime label

--- scripts/make-sdxl-runtime.sh
+++ scripts/make-sdxl-runtime.sh
@@ -2,4 +2,4 @@
 
 docker build -t soulteary/sdxl:runtime . -f docker/Dockerfile.runtime
 
-docker build -t soulteary/sdxl:xformers . -f docker/Dockerfile.runtime-xformers
+docker build -t soulteary/sdxl:runtime-xformers . -f docker/Dockerfile.runtime-xformers
